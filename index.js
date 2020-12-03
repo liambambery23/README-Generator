f97d535999c77df40e217b289596531f716ce240
Liam B <71141670+liambambery23@users.noreply.github.com>
2020-12-03 14:34:16 -0500

    created function to write data to a file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const inquirer = required('inquirer');
+const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
+
+const writeFileAsync = util.promisify(writeToFile);
+
 
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
@@ -14,6 +17,18 @@ const questions = [
         validate: function(answer) {
             if (answer.length < 1) {
                 return console.log("A valid GitHub username is required");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is your email?",
+        name: 'email',
+        default: 'liam.bambery23@gmail.com',
+        validate: function(answer) {
+            if (answer.length < 1) {
+                return console.log("A valid email is required");
             }
             return true;
         }
@@ -77,17 +92,35 @@ const questions = [
     {
         type: 'list',
         message: 'Choose a license for your project',
-        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        choices: ['MIT', 'ISC', 'Apache', 'Unlicense'],
         name: 'license',
     },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("Hooray! Your README.md file has been generated successfully!")
+    });
 }
+
 
 // function to initialize program
 function init() {
+    inquirer
+        .prompt(questions)
+        .then(function(answers){
+            console.log(answers);
+            //once I get my data, what do I do next
+            //call the data 
+            console.log(generateMarkdown(answers));
+            //write into the file
+            writeToFile("./utils/README.md",generateMarkdown(answers));
+        })
 
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,50 @@
+
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+
+  
+  return `# Readme Test: ${data.title}
+
+  ![license](https://img.shields.io/badge/license-${data.license}-green)
+
+  ## Description: 
+
+  ${data.description}
+
+  ## Table of Contents:
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+
+  ## Installation: 
+
+  ${data.installation}
+
+  ## Usage: 
+
+  ${data.usage}
+
+  ## Contributing: 
+
+  ${data.contributing}
+
+  ## Tests: 
+
+  ${data.tests}
+
+  ## Questions:
+
+  If you have any questions regarding this project, please contact me via email.
+  
+  ${data.email}
+
+  Github Username: [${data.username}](https://github.com/${data.username})
+
+
+  ## License:
+
+  This project uses the ${data.license} License
 
 `;
 }
